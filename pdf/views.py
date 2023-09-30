79a2e4815ea93b7892b8c9a9a160b93ed790b961
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-30 19:33:56 +0000

    feat: Add a view to render CV Creation Form

--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+
+# def create_cv(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name","")
+#         email = request.POST.get("email","")
+#         phone = request.POST.get("phone","")
+#         summary = request.POST.get("summary","")
+#         previous_work = request.POST.get("previous-work","")
+#         degree = request.POST.get("degree","")
+#         school = request.POST.get("school","")
+#         university = request.POST.get("university","")
+#         skills = request.POST.get("skills","")
+
+#         user_profile = Profile.objects.create(
+#             name=name, 
+#             email=email, 
+#             phone=phone,
+#             summary=summary, 
+#             previous_work=previous_work, 
+#             degree=degree, 
+#             university=university , 
+#             skills=skills,
+#             school=school
+#             )
+#     return render(request, "pdf/accept.html")
+
+
+def generated_cv_view(request,id):
+    user_profile = Profile.objects.get(pk=id)
+    return render(request, "pdf/resume.html",{ "user_profile":user_profile})
