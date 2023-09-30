8c149f3dd356cd104d765c35a113bb2c4bd6a846
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-30 19:36:40 +0000

    feat: Add PDF URLS

---
+++ pdf/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path("", views.create_cv, name="create_cv"),
+    path("resume/<int:id>", views.generated_cv_view, name="resume"),
+]
