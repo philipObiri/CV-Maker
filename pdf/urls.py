08b9b06f1d77312c599da0b242decb33813b942e
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-01 14:42:26 +0000

    feat: Add Endpoint for Viewing List Of Profiles

--- pdf/urls.py
+++ pdf/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.create_cv, name="create_cv"),
     path("resume/<int:id>", views.generated_cv_view, name="resume"),
+    path("profiles/", views.list_of_profiles, name="profiles"),
 ]
